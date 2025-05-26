cb0cd53d9274b552fbf6a601fd40192593f69289
LNenjoyer <ut_3779@zsp3zamosc.pl>
2025-05-26 13:23:35 +0200

    wygląd

--- AplikacjaDesktopowaINF04012301SG/MainPage.xaml.cs
+++ AplikacjaDesktopowaINF04012301SG/MainPage.xaml.cs
@@ -2,24 +2,20 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Genhas(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
+        private void Zatw(object sender, EventArgs e)
+        {
+
+        }
+
     }
 
 }
